7a0a2342281a2c3e6626cb525b90fe2b4d5fd0c0
Zoltán Király <zoliky@gmail.com>
2024-04-05 18:31:18 +0300

    Sync

--- config.sh
+++ config.sh
@@ -3,3 +3,13 @@
 echo "Install Xorg"
 doas pkg install -y xorg
 doas pw groupmod video -m zoliky
+
+#echo "Install Intel graphics"
+#doas install -y drm-kmod
+#doas sysrc kld_list+=i915kms
+
+echo "Install Xfce"
+doas pkg install -y xfce xfce4-goodies
+doas tee -a /etc/fstab <<EOF
+proc $(printf '\t\t\t')/proc$(printf '\t') procfs$(printf '\t')rw$(printf '\t\t')0$(printf '\t')
+EOF
