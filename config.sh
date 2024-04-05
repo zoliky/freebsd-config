282848f47381beca77dd13e337274e610eb4b041
Zoltán Király <zoliky@gmail.com>
2024-04-05 21:54:37 +0300

    Sync

--- config.sh
+++ config.sh
@@ -5,11 +5,15 @@ doas pkg install -y xorg
 doas pw groupmod video -m zoliky
 
 #echo "Install Intel graphics"
-#doas install -y drm-kmod
-#doas sysrc kld_list+=i915kms
+#doas install -y drm-kmod libva-intel-driver mesa-libs mesa-dri
+#doas sysrc -f /etc/rc.conf kld_list+=i915kms
 
 echo "Install Xfce"
 doas pkg install -y xfce xfce4-goodies
 doas tee -a /etc/fstab <<EOF
 proc $(printf '\t\t\t')/proc$(printf '\t')procfs$(printf '\t')rw$(printf '\t\t')0$(printf '\t')0
 EOF
+
+echo "Adjustments"
+doas mv /boot/loader.conf /boot/loader.conf.backup
+doas mv loader.conf /boot/
