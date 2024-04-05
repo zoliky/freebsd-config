a64d1e06dde26571d03f3aa9318119e59dadd219
Zoltán Király <zoliky@gmail.com>
2024-04-06 02:25:32 +0300

    Sync

--- config.sh
+++ config.sh
@@ -20,3 +20,6 @@ doas mv loader.conf /boot/
 
 echo "Install utilities"
 doas pkg install -y vim neovim htop neofetch rsync kitty tmux dfc zip mpv fzf meson
+
+echo "Install Firefox"
+doas pkg install -y firefox
