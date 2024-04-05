7b2d43a419626cb03e6ba6d7e161d874c44096d3
Zoltán Király <zoliky@gmail.com>
2024-04-05 18:33:36 +0300

    Sync

--- config.sh
+++ config.sh
@@ -11,5 +11,5 @@ doas pw groupmod video -m zoliky
 echo "Install Xfce"
 doas pkg install -y xfce xfce4-goodies
 doas tee -a /etc/fstab <<EOF
-proc $(printf '\t\t\t')/proc$(printf '\t') procfs$(printf '\t')rw$(printf '\t\t')0$(printf '\t')
+proc $(printf '\t\t\t')/proc$(printf '\t')procfs$(printf '\t')rw$(printf '\t\t')0$(printf '\t')0
 EOF
