efce0e44e12a2a5025266498b1e3d299f03617dc
Zoltán Király <zoliky@gmail.com>
2024-08-03 17:03:25 +0300

    Sync

--- packages.sh
+++ packages.sh
@@ -2,6 +2,6 @@
 
 echo "Install packages"
 doas pkg install -y audacity blender calibre chromium darktable emacs filezilla \
-     flacon foliate gimp inkscape libreoffice lollypop obs-studio sox thunderbird \
-     tigervnc-viewer transmission-gtk vlc xournalpp en-hunspell ro-hunspell hu-hunspell \
-     nerd-fonts
+     flacon foliate gimp inkscape kid3-qt6 libreoffice lollypop obs-studio sox \
+     thunderbird tigervnc-viewer transmission-gtk vlc xournalpp en-hunspell \
+     ro-hunspell hu-hunspell nerd-fonts
