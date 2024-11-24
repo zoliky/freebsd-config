cf08a2eaa5e1916082faff45e447015af3fa7772
Zoltán Király <zoliky@gmail.com>
2024-11-24 17:26:58 +0200

    Sync

--- packages.sh
+++ packages.sh
@@ -1,7 +1,38 @@
 #!/bin/sh
 
+set -- \
+    aisleriot \
+    audacity \
+    calibre \
+    chromium \
+    darktable \
+    emacs \
+    en-hunspell \
+    evince-lite \
+    filezilla \
+    flacon \
+    foliate \
+    gimp \
+    gnome-mahjongg \
+    hu-hunspell \
+    inkscape \
+    isync \
+    kid3-qt6 \
+    libreoffice \
+    lollypop \
+    mu4e \
+    nerd-fonts \
+    obs-studio \
+    ro-hunspell \
+    sox \
+    thunderbird \
+    tigervnc-viewer \
+    transmission-gtk \
+    vlc \
+    vscode \
+    xournalpp
+
 echo "Install packages"
-doas pkg install -y aisleriot audacity calibre chromium darktable emacs evince-lite \
-     filezilla flacon foliate gimp gnome-mahjongg inkscape kid3-qt6 libreoffice \
-     lollypop obs-studio sox thunderbird tigervnc-viewer transmission-gtk vlc \
-     xournalpp en-hunspell ro-hunspell hu-hunspell nerd-fonts
+for package in "$@"; do
+  doas pkg install -y "$package"
+done
