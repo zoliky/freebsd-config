5d31af510f465d9e17094c410289609d2a39baa5
Zoltán Király <zoliky@gmail.com>
2024-11-27 18:45:50 +0200

    Sync

--- config.sh
+++ config.sh
@@ -2,35 +2,35 @@
 
 username=zoliky
 
-echo "Ensure packages are up-to-date"
+# Update repository catalog and upgrade packages
 doas pkg update && doas pkg upgrade -y
 
-echo "Install Xorg"
+# Install X.Org
 doas pkg install -y xorg
 
-echo "Install Intel graphics"
+# Install Intel graphics and enable i915kms at boot
 doas pkg install -y drm-kmod libva-intel-driver mesa-libs mesa-dri
 doas sysrc kld_list+=i915kms
 
-echo "Enable dynamic adjustment of CPU frequency"
+# Enable powerd and set flags: hiadaptive on AC, adaptive on battery
 doas sysrc powerd_enable="YES"
 doas sysrc powerd_flags="-a hiadaptive -b adaptive"
 
-echo "Adjustments"
+# Copy custom configuration files
 doas cp /boot/loader.conf /boot/loader.conf.backup
 doas cp loader.conf /boot/
 
 doas cp devfs.rules /etc/
 doas sysrc devfs_system_ruleset="system"
 
-echo "Install utilities"
+# Install utilities
 doas pkg install -y vim htop fastfetch rsync kitty tmux dfc zip mpv yt-dlp fzf meson mixertui wget
 
-echo "Install Firefox"
+# Install Firefox
 doas pkg install -y firefox
 doas chown -R $username:$username ~/.cache
 
-echo "Configure sound"
+# Configure USB audio (Behringer UCA202)
 doas tee -a /etc/sysctl.conf <<EOF
 # USB audio
 hw.snd.default_unit=3
@@ -38,40 +38,40 @@ hw.snd.default_auto=0
 hw.snd.vpc_autoreset=0
 EOF
 
-echo "Enhance desktop responsiveness under high CPU use"
+# Set a threshold value more suitable for desktop use
 doas tee -a /etc/sysctl.conf <<EOF
-# Enhance desktop responsiveness under high CPU use
+# Set a threshold value more suitable for desktop use
 kern.sched.preempt_thresh=200
 EOF
 
-echo "Install fonts"
+# Install fonts
 doas pkg install -y hack-font
 
-echo "Install Xfce"
+# Install Xfce with additional packages and enable D-BUS
 doas pkg install -y xfce xfce4-goodies xfce4-pulseaudio-plugin plank xarchiver networkmgr redshift galculator xdg-user-dirs
 doas tee -a /etc/fstab <<EOF
 proc $(printf '\t\t\t')/proc$(printf '\t')procfs$(printf '\t')rw$(printf '\t\t')0$(printf '\t')0
 EOF
 doas sysrc dbus_enable="YES"
 
-echo "Install and enable LightDM"
+# Install and enable LightDM display manager
 doas pkg install -y lightdm lightdm-gtk-greeter
 doas sysrc lightdm_enable="YES"
 
-echo "Configure webcam"
+# Enable webcamd for USB webcam, Wacom tablet, and other devices
 doas pkg install -y webcamd
 doas sysrc webcamd_enable="YES"
 doas sysrc kld_list+="cuse"
 
-echo "Configure Wacom tablet"
+# Install packages for Wacom tablet support
 doas pkg install -y libwacom xf86-input-wacom
 
-echo "Add user to core groups"
+# Add user to core groups
 doas pw groupmod operator -m $username
 doas pw groupmod realtime -m $username
 doas pw groupmod video -m $username
 doas pw groupmod webcamd -m $username
 doas pw groupmod network -m $username
 
-echo "Clear local package cache"
+# Clear local package cache
 doas pkg clean -y
