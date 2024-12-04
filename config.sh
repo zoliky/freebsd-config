d1b0acb0ee98eac2c7fa12b46f8da90e3e4b07e7
Zoltán Király <zoliky@gmail.com>
2024-12-04 04:16:56 +0200

    Sync

--- config.sh
+++ config.sh
@@ -53,9 +53,6 @@ copy_config_file() {
 # Update FreeBSD repository catalog and upgrade packages
 doas pkg update && doas pkg upgrade -y
 
-# Install git
-install_packages git
-
 # Install the Ports Collection if not present
 if [ ! -d "/usr/ports" ] || [ -z "$(ls -A /usr/ports)" ]; then
   doas git clone --depth 1 https://git.FreeBSD.org/ports.git -b 2024Q4 /usr/ports
